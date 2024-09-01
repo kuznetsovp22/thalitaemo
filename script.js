ef76ed6e9c5b775d94c6c9252e6e05ae6c999321
kuznetsovp22 <kzzin.tv@gmail.com>
2024-09-01 12:40:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('surprise-button').addEventListener('click', function() {
+    const surprise = document.getElementById('surprise');
+    surprise.classList.toggle('hidden');
+});
